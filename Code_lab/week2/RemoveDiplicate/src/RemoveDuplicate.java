6e1f308bd3a104aa91d3ad552d925b549434f513
Qinyi Zhang <alessa@QinyideMacBook-Pro.local>
2020-09-30 02:43:51 -0700

    add week2

---
+++ Code_lab/week2/RemoveDiplicate/src/RemoveDuplicate.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Stack;
+
+public class RemoveDuplicate {
+    public String removeDuplicate(String s){
+        Stack<Character> stack = new Stack();
+        HashSet<Character> seen = new HashSet<>();
+        HashMap<Character, Integer> last_occurence = new HashMap<>();
+        for (int i = 0; i < s.length(); i++){
+            last_occurence.put(s.charAt(i), i);
+        }
+        for (int i =0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if (!seen.contains(c)){
+                while(!stack.isEmpty() && c < stack.peek() && last_occurence.get(stack.peek()) > i){
+                    seen.remove(stack.pop());
+                }
+                seen.add(c);
+                stack.push(c);
+            }
+        }
+        StringBuilder sb = new StringBuilder(stack.size());
+        for (Character c: stack) sb.append((c.charValue()));
+        return sb.toString();
+    }
+}
